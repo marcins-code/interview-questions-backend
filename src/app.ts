485706d1e4661d277b8b05850afb27354cce079d
marcin's-code <mpaczkowski40@gmail.com>
2021-10-09 10:43:07 +0200

    Add cors and json

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,15 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 // Boot express
 const app: Application = express();
 
-// Application routing
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+app.use(express.json());
 app.use('/', (req: Request, res: Response, _next: NextFunction) => {
-  res.status(200).send({ data: 'Hello from Oranio AS' });
+  res.status(200).json({ data: 'Hello from Oranio AS' });
 });
 
 export default app;
